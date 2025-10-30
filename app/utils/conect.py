16633c847f04bc40118ac944549914102b2cb2fe
Fernando José De Celis Hurtado <ferdehur@alum.us.es>
2025-10-30 19:50:51 +0100

    fix: comments eliminated

--- app/utils/conect.py
+++ app/utils/conect.py
@@ -5,8 +5,6 @@ def connect(options: SpaceConnectionOptions) -> SpaceClient:
     """
     Conecta usando SpaceConnectionOptions. Los valores de `url`, `api_key` y `timeout` son obligatorios.
     """
-    # Validar los parámetros de conexión
     options.validate()
 
-    # Si los valores son correctos, creamos el SpaceClient
     return SpaceClient(url=options.url, api_key=options.api_key, timeout=options.timeout)

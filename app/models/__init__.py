844a8d28a5065363ed7e8c76c7af11ac4fd21516
daniel <d4niel.bh@gmail.com>
2025-11-26 19:21:35 +0100

    Revert "Feat/modelos"

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .contracts import *
-from .feature_eval_result import *
-from .service_context_enums import *
-from .service_context import *
--- app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import *
-from .contract_module import *
-from .feature_eval_module import *
-from .service_context_module import *

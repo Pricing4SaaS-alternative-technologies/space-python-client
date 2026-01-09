3feea447c8356cd5cb5652ffab0420b8f8601707
d4niel980 <danbenhid@alum.us.es>
2026-01-09 20:06:35 +0100

    fix: Updated tests for testing the correct status reception from the contract_module
    A new test has been added to the contract_module test class to test the 404 status received from trying to get the contracts for a inexistent user_id
    
    #46

--- tests/test_contract_module.py
+++ tests/test_contract_module.py
@@ -201,6 +201,84 @@ plans:
 		assert contract["userContact"]["userId"] == user_id
 		print("Test OK :) user contract correctly retrieved:", contract)
 
+	@pytest.mark.asyncio
+	async def test_get_user_non_existent_id_contract_from_client(self, space_client):
+		#Creación del servicio dummy sobre el que generar el contrato
+		service_name = f"Service_{uuid.uuid4().hex[:8]}"
+		yaml = f"""saasName: {service_name}
+syntaxVersion: "3.0"
+version: "1.0.0"
+createdAt: "2025-01-01"
+currency: USD
+features:
+  basic:
+    description: Test
+    valueType: BOOLEAN
+    defaultValue: true
+    type: DOMAIN
+plans:
+  BASIC:
+    description: Basic plan
+    price: 0.0
+    unit: user/month
+    features: null
+    usageLimits: null"""
+    
+		with tempfile.NamedTemporaryFile(mode='w', suffix='.yml', delete=False) as f:
+			f.write(yaml)
+			temp_path = f.name
+		try:
+			await space_client.service_context.add_service(temp_path)
+		finally:
+			try:
+				os.unlink(temp_path)
+			except:
+				pass
+		user_id = uuid.uuid4().hex[:8]
+		user_name = f"user_{user_id}"
+		contract_to_create = {
+			"userContact": {
+				"userId": user_id,
+				"username": user_name
+			},
+			"billingPeriod": {
+				"autoRenew": True,
+				"renewalDays": 30
+			},
+			"contractedServices": {
+				service_name: "1.0.0"
+       		},
+			"subscriptionPlans": {	
+				service_name: "BASIC"
+			},
+			"subscriptionAddOns": {}
+		}
+		contract_to_create2 = {
+			"userContact": {
+				"userId": user_id,
+				"username": user_name
+			},
+			"billingPeriod": {
+				"autoRenew": True,
+				"renewalDays": 45
+			},
+			"contractedServices": {
+				service_name: "1.0.0"
+       		},
+			"subscriptionPlans": {	
+				service_name: "BASIC"
+			},
+			"subscriptionAddOns": {}
+		}
+		#Generamos el contrato y comprobamos que existe
+		await space_client.contracts.add_contract(contract_to_create)
+		#await space_client.contracts.add_contract(contract_to_create2)
+		try:
+			await space_client.contracts.get_user_id_contract(3)
+		except Exception as e:
+			assert str(e).__contains__("404")
+			print("Test OK :) Caught expected exception for non-existent user ID:", e)
+
     
 	@pytest.mark.asyncio
 	async def test_contract_correct_update(self, space_client):
